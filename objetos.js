1b25b38d8e4bbf4557e0f37edea969aa8239d624
Pablo Pineda <pinedapablo716@gmail.com>
2022-01-14 15:44:37 -0500

    prueba

---
+++ objetos.js
@@ -0,0 +1,12 @@
+var miAuto ={
+    marca: 'Audi',
+    modelo: 'A8',
+    color: 'Rojo',
+    annio: 2020,
+    detalleAuto : function(){
+        console.log(`La marca es ${this.marca}, el modelo ${this.modelo}, el año es ${this.annio}`)
+    }
+}
+
+miAuto.detalleAuto()
+
